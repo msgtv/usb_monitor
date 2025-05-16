c1779814b925e75a5057c8d13f2a88c3be927dfe
msgtv <linar_97@list.ru>
2025-05-16 21:05:30 +0500

    usbsearargsdepend updated

--- app/usbs/dependencies.py
+++ app/usbs/dependencies.py
@@ -8,6 +8,8 @@ class UsbSearchArgsDepend:
             self,
             class_type: Tuple[int] = Query(None, description="Класс USB-устройства (например, 1299, 1, 1008)"),
             is_accepted: bool = None,
+            department_id: int = Query(None, ge=1),
     ):
         self.class_types = class_type
         self.is_accepted = is_accepted
+        self.department_id = department_id

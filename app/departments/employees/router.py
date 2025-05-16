92e6a823ddbf2072b9989c181a6e43405900b28b
lmsgtv <linar_97@list.ru>
2025-05-16 16:54:50 +0500

    get_employee endpoint

--- app/departments/employees/router.py
+++ app/departments/employees/router.py
@@ -1,10 +1,10 @@
-from typing import List
-
 from fastapi import APIRouter
 from fastapi_pagination import Page
 
 from app.departments.employees.dao import EmployeeDAO
 from app.departments.employees.schemas import SEmployee
+from app.departments.employees.exceptions import EmployeeNotFound
+
 
 router = APIRouter(
     prefix="/employees",
@@ -16,3 +16,11 @@ async def get_employees() -> Page[SEmployee]:
     employees = await EmployeeDAO.get_all_paginated()
 
     return employees
+
+
+@router.get('/{employee_id}')
+async def get_employee(employee_id: int) -> SEmployee:
+    employee = await EmployeeDAO.get_by_id(employee_id)
+    if employee:
+        return employee
+    raise EmployeeNotFound(f'No employee with id {employee_id}')

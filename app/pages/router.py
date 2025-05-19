8ea2e4f024404c2d02886186fee7432af78c4d9b
lmsgtv <linar_97@list.ru>
2025-05-19 16:26:59 +0500

    frontend router

--- app/pages/router.py
+++ app/pages/router.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter, Request, Depends
 from fastapi.templating import Jinja2Templates
+from fastapi_pagination import Page, Params
+from fastapi_pagination.api import pagination_ctx
 
 from app.events.router import get_events_detailed
 
@@ -32,10 +34,46 @@ async def get_events_page(
 
 
 @router.get('/tasks')
-async def get_events_page(
+async def get_tasks_page(
         request: Request,
 ):
     return templates.TemplateResponse(
         name='tasks/main_page.html',
+        context={'request': request},
+    )
+
+@router.get('/tasks/add')
+async def get_tasks_add_page(
+        request: Request,
+):
+    return templates.TemplateResponse(
+        name='tasks/add/main_page.html',
+        context={'request': request},
+    )
+
+@router.get('/computers')
+async def get_computers_page(
+        request: Request,
+):
+    return templates.TemplateResponse(
+        name='computers/main_page.html',
+        context={'request': request},
+    )
+
+@router.get('/usbs')
+async def get_usbs_page(
+        request: Request,
+):
+    return templates.TemplateResponse(
+        name='usbs/main_page.html',
+        context={'request': request},
+    )
+
+@router.get('/employees')
+async def get_employees_page(
+        request: Request,
+):
+    return templates.TemplateResponse(
+        name='employees/main_page.html',
         context={'request': request},
     )
